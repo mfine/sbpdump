cf0cad1090636b7d42f342f12deb2d8309e70a1e
Mark Fine <mark.fine@gmail.com>
2019-05-18 19:27:17 -0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -31,60 +31,65 @@ struct Msg {
 }
 
 fn msg(value: &Value) -> Option<Msg> {
-    value["msg_type"].as_u64()
-        .and_then(|msg_type|
-                  match msg_type {
-                      74 =>
-                          value["header"]["t"]["tow"].as_u64()
-                          .and_then(|tow|
-                                    value["obs"].as_array()
-                                    .and_then(|obs| {
-                                        let mut sid_vec: Vec<Sid> = Vec::new();
-                                        for ob in obs.iter() {
-                                            if let Some(sat) = ob["sid"]["sat"].as_u64() {
-                                                if let Some(code) = ob["sid"]["code"].as_u64() {
-                                                    sid_vec.push(Sid { sat, code })
-                                                }
-                                            }
-                                        }
-                                        Some(Msg { tow: tow / 1000, msg_type, sid_vec })})),
-                      138 =>
-                          value["common"]["toe"]["tow"].as_u64()
-                          .and_then(|tow|
-                                    value["common"]["sid"]["sat"].as_u64()
-                                    .and_then(|sat|
-                                              value["common"]["sid"]["code"].as_u64()
-                                              .and_then(|code| Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
-                      149 =>
-                          value["common"]["toe"]["tow"].as_u64()
-                          .and_then(|tow|
-                                    value["common"]["sid"]["sat"].as_u64()
-                                    .and_then(|sat|
-                                              value["common"]["sid"]["code"].as_u64()
-                                              .and_then(|code| Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
-                      1501 =>
-                          value["time"]["tow"].as_u64()
-                          .and_then(|tow|
-                                    value["sid"]["sat"].as_u64()
-                                    .and_then(|sat|
-                                              value["sid"]["code"].as_u64()
-                                              .and_then(|code| Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
-                      1505 =>
-                          value["time"]["tow"].as_u64()
-                          .and_then(|tow|
-                                    value["sid"]["sat"].as_u64()
-                                    .and_then(|sat|
-                                              value["sid"]["code"].as_u64()
-                                              .and_then(|code| Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
-                      1510 =>
-                          value["time"]["tow"].as_u64()
-                          .and_then(|tow|
-                                    value["sid"]["sat"].as_u64()
-                                    .and_then(|sat|
-                                              value["sid"]["code"].as_u64()
-                                              .and_then(|code| Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
-                      _ => None,
-                  })
+    value["msg_type"].as_u64().and_then(
+        |msg_type|
+        match msg_type {
+            74 =>
+                value["header"]["t"]["tow"].as_u64().and_then(
+                    |tow|
+                    value["obs"].as_array().and_then(|obs| {
+                        let mut sid_vec: Vec<Sid> = Vec::new();
+                        for ob in obs.iter() {
+                            if let Some(sat) = ob["sid"]["sat"].as_u64() {
+                                if let Some(code) = ob["sid"]["code"].as_u64() {
+                                    sid_vec.push(Sid { sat, code })
+                                }
+                            }
+                        }
+                        Some(Msg { tow: tow / 1000, msg_type, sid_vec })
+                    })),
+            138 =>
+                value["common"]["toe"]["tow"].as_u64().and_then(
+                    |tow|
+                    value["common"]["sid"]["sat"].as_u64().and_then(
+                        |sat|
+                        value["common"]["sid"]["code"].as_u64().and_then(
+                            |code|
+                            Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
+            149 =>
+                value["common"]["toe"]["tow"].as_u64().and_then(
+                    |tow|
+                    value["common"]["sid"]["sat"].as_u64().and_then(
+                        |sat|
+                        value["common"]["sid"]["code"].as_u64().and_then(
+                            |code|
+                            Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
+            1501 =>
+                value["time"]["tow"].as_u64().and_then(
+                    |tow|
+                    value["sid"]["sat"].as_u64().and_then(
+                        |sat|
+                        value["sid"]["code"].as_u64().and_then(
+                            |code|
+                            Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
+            1505 =>
+                value["time"]["tow"].as_u64().and_then(
+                    |tow|
+                    value["sid"]["sat"].as_u64().and_then(
+                        |sat|
+                        value["sid"]["code"].as_u64().and_then(
+                            |code|
+                            Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
+            1510 =>
+                value["time"]["tow"].as_u64().and_then(
+                    |tow|
+                    value["sid"]["sat"].as_u64().and_then(
+                        |sat|
+                        value["sid"]["code"].as_u64().and_then(
+                            |code|
+                            Some(Msg { tow, msg_type, sid_vec: vec![Sid { sat, code }] })))),
+            _ => None,
+        })
 }
 
 fn main() -> Result<(), Error> {
@@ -114,7 +119,9 @@ fn main() -> Result<(), Error> {
     for (tow, msg_type_map) in tow_map.iter() {
         for (msg_type, sid_vec) in msg_type_map.iter() {
             let mut sid_vec_sort = sid_vec.to_vec();
-            sid_vec_sort.retain(|sid| [0, 1, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 56, 57, 58, 61].contains(&sid.code));
+            sid_vec_sort.retain(
+                |sid|
+                [0, 1, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 56, 57, 58, 61].contains(&sid.code));
             sid_vec_sort.sort();
             sid_vec_sort.dedup();
             let mut sid_vec_str = String::new();
