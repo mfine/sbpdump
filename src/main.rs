2ac3ffa1595241d788626d65ffbff5954d47f667
Mark Fine <mark.fine@gmail.com>
2019-05-21 20:55:10 -0700

    sets.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde_json::Value;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet, HashSet};
 use std::env;
 use std::fmt;
 use std::fs::File;
@@ -12,6 +12,11 @@ const MSG_EPHEMERIS_GAL: u64 = 149;
 const MSG_SSR_ORBIT_CLOCK: u64 = 1501;
 const MSG_SSR_CODE_BIASES: u64 = 1505;
 const MSG_SSR_PHASE_BIASES: u64 = 1510;
+
+const GPS_CODES: [u64; 12] = [0, 1, 5, 6, 7, 8, 9, 10, 11, 56, 57, 58];
+const GAL_CODES: [u64; 16] = [
+    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 61,
+];
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Clone)]
 struct Sid {
@@ -29,18 +34,18 @@ struct Msg {
     msg_type: u64,
     sender: u64,
     tow: u64,
-    sid_vec: Vec<Sid>,
+    sid_set: BTreeSet<Sid>,
 }
 
 impl Msg {
     fn observations(value: &Value, msg_type: u64, sender: u64) -> Option<Msg> {
         value["header"]["t"]["tow"].as_u64().and_then(|tow| {
             value["obs"].as_array().and_then(|obs| {
-                let mut sid_vec: Vec<Sid> = Vec::new();
+                let mut sid_set: BTreeSet<Sid> = BTreeSet::new();
                 for ob in obs.iter() {
                     if let Some(sat) = ob["sid"]["sat"].as_u64() {
                         if let Some(code) = ob["sid"]["code"].as_u64() {
-                            sid_vec.push(Sid { sat, code })
+                            sid_set.insert(Sid { sat, code });
                         }
                     }
                 }
@@ -48,7 +53,7 @@ impl Msg {
                     msg_type,
                     sender,
                     tow: tow / 1000,
-                    sid_vec,
+                    sid_set,
                 })
             })
         })
@@ -58,11 +63,13 @@ impl Msg {
         value["common"]["toe"]["tow"].as_u64().and_then(|tow| {
             value["common"]["sid"]["sat"].as_u64().and_then(|sat| {
                 value["common"]["sid"]["code"].as_u64().and_then(|code| {
+                    let mut sid_set: BTreeSet<Sid> = BTreeSet::new();
+                    sid_set.insert(Sid { sat, code });
                     Some(Msg {
                         msg_type,
                         sender,
                         tow,
-                        sid_vec: vec![Sid { sat, code }],
+                        sid_set,
                     })
                 })
             })
@@ -73,11 +80,13 @@ impl Msg {
         value["time"]["tow"].as_u64().and_then(|tow| {
             value["sid"]["sat"].as_u64().and_then(|sat| {
                 value["sid"]["code"].as_u64().and_then(|code| {
+                    let mut sid_set: BTreeSet<Sid> = BTreeSet::new();
+                    sid_set.insert(Sid { sat, code });
                     Some(Msg {
                         msg_type,
                         sender,
                         tow,
-                        sid_vec: vec![Sid { sat, code }],
+                        sid_set,
                     })
                 })
             })
@@ -110,40 +119,40 @@ fn main() -> Result<(), Error> {
     let input = File::open(filename)?;
     let buffered = BufReader::new(input);
 
-    let mut tow_map: BTreeMap<u64, BTreeMap<u64, BTreeMap<u64, Vec<Sid>>>> = BTreeMap::new();
+    let mut tow_map: BTreeMap<u64, BTreeMap<u64, BTreeMap<u64, BTreeSet<Sid>>>> = BTreeMap::new();
     for line in buffered.lines() {
         let value: Value = serde_json::from_str(&(line?))?;
         if let Some(msg) = Msg::new(&value) {
-            let mut sid_vec = msg.sid_vec;
+            let mut sid_set = msg.sid_set;
             tow_map
                 .entry(msg.tow)
                 .or_insert_with(BTreeMap::new)
                 .entry(msg.sender)
                 .or_insert_with(BTreeMap::new)
                 .entry(msg.msg_type)
-                .or_insert_with(Vec::new)
-                .append(&mut sid_vec);
+                .or_insert_with(BTreeSet::new)
+                .append(&mut sid_set);
         }
+    }
+
+    let mut code_set: HashSet<u64> = HashSet::new();
+    for code in GPS_CODES.iter() {
+        code_set.insert(*code);
+    }
+    for code in GAL_CODES.iter() {
+        code_set.insert(*code);
     }
 
     for (tow, sender_map) in tow_map.iter() {
         for (sender, msg_type_map) in sender_map.iter() {
-            for (msg_type, sid_vec) in msg_type_map.iter() {
-                let mut sid_vec_sort = sid_vec.to_vec();
-                sid_vec_sort.retain(|sid| {
-                    [
-                        0, 1, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-                        25, 26, 27, 28, 56, 57, 58, 61,
-                    ]
-                    .contains(&sid.code)
-                });
-                sid_vec_sort.sort();
-                sid_vec_sort.dedup();
-                let mut sid_vec_str = String::new();
-                for sid in sid_vec_sort.iter() {
-                    sid_vec_str.push_str(&format!("{} ", sid));
+            for (msg_type, sid_set) in msg_type_map.iter() {
+                let mut sid_set_str = String::new();
+                for sid in sid_set.iter() {
+                    if code_set.contains(&sid.code) {
+                        sid_set_str.push_str(&format!("{} ", sid));
+                    }
                 }
-                println!("{:>6} {:>5} {:>4} {}", tow, sender, msg_type, sid_vec_str);
+                println!("{:>6} {:>5} {:>4} {}", tow, sender, msg_type, sid_set_str);
             }
         }
         println!();
